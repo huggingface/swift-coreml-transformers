2b50d46898ba910ab7fe2c090a9100e42b1f77c8
Julien Chaumond <chaumond@gmail.com>
2019-06-27 19:34:49 -0400

    Also test performance
    Co-Authored-By: Lysandre Debut <lysandre.debut@reseau.eseo.fr>

--- CoreMLBertTests/BertForQATests.swift
+++ CoreMLBertTests/BertForQATests.swift
@@ -61,7 +61,7 @@ class BertForQATests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            _ = m.predict(question: question, context: context)
         }
     }
 
