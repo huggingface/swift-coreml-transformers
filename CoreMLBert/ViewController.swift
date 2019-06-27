a6ee8a0ce8cef1198c04a80536b39fd646fd6f46
Julien Chaumond <chaumond@gmail.com>
2019-06-27 09:50:41 -0400

    Initial Commit

---
+++ CoreMLBert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLBert
+//
+//  Created by Julien Chaumond on 27/06/2019.
+//  Copyright © 2019 Hugging Face. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
