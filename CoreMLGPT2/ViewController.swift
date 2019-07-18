229bdad6a69ed8094d87082f44da4e84d0e6c7d3
Julien Chaumond <chaumond@gmail.com>
2019-07-18 15:40:30 -0400

    Scaffolding for GPT-2
    Co-Authored-By: Lysandre Debut <lysandre.debut@reseau.eseo.fr>

---
+++ CoreMLGPT2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLGPT2
+//
+//  Created by Julien Chaumond on 18/07/2019.
+//  Copyright © 2019 Hugging Face. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
