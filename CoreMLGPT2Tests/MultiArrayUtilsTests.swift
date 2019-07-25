5e7f73a7a790b156cd5eea0a1029917b5cdfcdf8
Julien Chaumond <chaumond@gmail.com>
2019-07-25 18:18:58 -0400

    [GPT-2] Top-K implementation
    cc @lysandrejik
    
    Co-Authored-By: Lysandre Debut <lysandre.debut@reseau.eseo.fr>

--- CoreMLGPT2Tests/MultiArrayUtilsTests.swift
+++ CoreMLGPT2Tests/MultiArrayUtilsTests.swift
@@ -17,8 +17,39 @@ class MultiArrayUtilsTests: XCTestCase {
         
         let o = MLMultiArray.slice(arr, sliceDim: 1, selectDims: [0: 0, 2: 0])
         print(o.debug)
-        
+        XCTAssertEqual(
+            MLMultiArray.toDoubleArray(o),
+            [2, 6, 10]
+        )
         let oo = MLMultiArray.slice(arr, indexing: [.select(0), .slice, .select(0)])
         print(oo.debug)
+        XCTAssertEqual(
+            MLMultiArray.toDoubleArray(oo),
+            [2, 6, 10]
+        )
+    }
+    
+    func testTopK() {
+        let arr: [Double] = Array(0..<4).map { Double($0) } + Array(30..<36).map { Double($0) }
+        XCTAssertEqual(
+            arr,
+            [0.0, 1.0, 2.0, 3.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0]
+        )
+        let topk = Math.topK(arr: arr, k: 6)
+        XCTAssertEqual(
+            topk.indexes,
+            [9, 8, 7, 6, 5, 4]
+        )
+        XCTAssertEqual(
+            topk.probs,
+            [0.6336913, 0.233122, 0.085760795, 0.031549633, 0.011606461, 0.0042697783]
+        )
+        
+        let sampleIndex = Math.sample(indexes: topk.indexes, probs: topk.probs)
+        print("sampleIndex", sampleIndex)
+        
+        XCTAssertTrue(
+            sampleIndex >= 4 && sampleIndex <= 9
+        )
     }
 }
--- Sources/Math.swift
+++ Sources/Math.swift
@@ -54,4 +54,82 @@ struct Math {
         let ptr = UnsafeMutablePointer<Double>(OpaquePointer(multiArray.dataPointer))
         return Math.argmax(ptr, count: multiArray.count)
     }
+    
+    /// Helper for top-k
+    /// 
+    static func topK(arr: [Double], k: Int) -> (indexes: [Int], probs: [Float]) {
+        let x = Array(arr.enumerated().map { ($0, $1) }
+            .sorted(by: { a, b -> Bool in a.1 > b.1 })
+            .prefix(through: min(k, arr.count) - 1))
+        let indexes = x.map { $0.0 }
+        let logits = x.map { Float($0.1) }
+        let probs = softmax(logits)
+        return (indexes: indexes, probs: probs)
+    }
+    
+    /// Multinomial sampling from an array of probs. Works well with topK
+    static func sample(indexes: [Int], probs: [Float]) -> Int {
+        let i = randomNumber(probabilities: probs)
+        return indexes[i]
+    }
+    
+    /**
+     Computes the "softmax" function over an array.
+     Based on code from https://github.com/nikolaypavlov/MLPNeuralNet/
+     This is what softmax looks like in "pseudocode" (actually using Python
+     and numpy):
+     x -= np.max(x)
+     exp_scores = np.exp(x)
+     softmax = exp_scores / np.sum(exp_scores)
+     First we shift the values of x so that the highest value in the array is 0.
+     This ensures numerical stability with the exponents, so they don't blow up.
+     */
+    static func softmax(_ x: [Float]) -> [Float] {
+        var x = x
+        let len = vDSP_Length(x.count)
+        
+        // Find the maximum value in the input array.
+        var max: Float = 0
+        vDSP_maxv(x, 1, &max, len)
+        
+        // Subtract the maximum from all the elements in the array.
+        // Now the highest value in the array is 0.
+        max = -max
+        vDSP_vsadd(x, 1, &max, &x, 1, len)
+        
+        // Exponentiate all the elements in the array.
+        var count = Int32(x.count)
+        vvexpf(&x, x, &count)
+        
+        // Compute the sum of all exponentiated values.
+        var sum: Float = 0
+        vDSP_sve(x, 1, &sum, len)
+        
+        // Divide each element by the sum. This normalizes the array contents
+        // so that they all add up to 1.
+        vDSP_vsdiv(x, 1, &sum, &x, 1, len)
+        
+        return x
+    }
+    
+    /// Multinomial sampling
+    ///
+    /// From https://stackoverflow.com/questions/30309556/generate-random-numbers-with-a-given-distribution
+    ///
+    static func randomNumber(probabilities: [Float]) -> Int {
+        // Sum of all probabilities (so that we don't have to require that the sum is 1.0):
+        let sum = probabilities.reduce(0, +)
+        // Random number in the range 0.0 <= rnd < sum :
+        let rnd = sum * Float(arc4random_uniform(UInt32.max)) / Float(UInt32.max)
+        // Find the first interval of accumulated probabilities into which `rnd` falls:
+        var accum: Float = 0.0
+        for (i, p) in probabilities.enumerated() {
+            accum += p
+            if rnd < accum {
+                return i
+            }
+        }
+        // This point might be reached due to floating point inaccuracies:
+        return (probabilities.count - 1)
+    }
 }
