22b0f3278e9da37676c9ed21c9900977a229013d
Julien Chaumond <chaumond@gmail.com>
2019-10-09 16:39:18 -0400

    Delete full_model_test.py

--- model_generation/full_model_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from pytorch_transformers import GPT2Model
-import numpy as np
-
-sequence_length = 3
-
-input_sequence = torch.tensor(np.zeros(sequence_length), dtype=torch.long).unsqueeze(0)
-
-GPT2Model.from_pretrained('gpt2')(input_sequence)
